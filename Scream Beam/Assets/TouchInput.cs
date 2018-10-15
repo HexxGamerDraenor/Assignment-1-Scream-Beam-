e45a10f786be0e95e4630042d4d58990e7362706
Skyla Moore <s.moore5104@gmail.com>
2018-10-15 12:59:39 +0100

    Unity Project upload

---
+++ Scream Beam/Assets/TouchInput.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class TouchInput : MonoBehaviour {
+    public float moveSpeed = 20f;
+
+    
+    void Update() {
+        //Touch input controls
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Camera.main.ScreenToWorldPoint(touch.position);
+        }
+
+        //PC input controls FOR TESTING WITHOUT ANDROID DEVICE
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2
+                (Input.GetAxis("Horizontal") * moveSpeed, 
+                Input.GetAxis("Vertical") * moveSpeed));
+        }
+	}
+}
