e946d0f6c2d3a6208a6436901600536d8638a6e3
Skyla Moore <s.moore5104@gmail.com>
2018-10-17 15:41:19 +0100

    UI update, added fire button

---
+++ Scream Beam/Assets/Scripts/FireScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireScript : MonoBehaviour {
+    public GameObject BeamSprite;
+
+    private bool isFiring = false;
+    private void Update()
+    {
+        if (isFiring == false)
+        {
+            BeamSprite.SetActive(false);
+        }
+    }
+
+	//function for setting beam active
+
+    public void Fire()
+    {
+        isFiring = true;
+        BeamSprite.SetActive(true);
+    }
+    public void ReleaseFire()
+    {
+        isFiring = false;
+    }
+}
