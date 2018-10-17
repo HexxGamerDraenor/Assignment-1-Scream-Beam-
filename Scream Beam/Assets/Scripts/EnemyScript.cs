b0761b670340d42ab994a361f51ad86af6fb6be4
Skyla Moore <s.moore5104@gmail.com>
2018-10-17 16:12:50 +0100

    Working on enemy movement

---
+++ Scream Beam/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+    public float moveAmount = 0.2f;
+    [SerializeField]
+    private Rigidbody2D Enemy;
+
+    public bool canMove = true;
+    private float timer;
+    public float timerResetTime = 2;
+	// Use this for initialization
+	void Start () {
+        timer = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (moveAmount > 0)
+        {
+            if (timer + timerResetTime >= Time.time)
+            {
+                return;
+            }
+            timer = Time.time + timerResetTime;
+
+            StartCoroutine("MoveTheEnemy()");
+        }
+    }
+
+    void MoveEnemy()
+    {
+        Enemy.AddForce(Vector3.down * moveAmount, ForceMode2D.Impulse);
+        canMove = false;
+        if(timer + timerResetTime >= Time.time)
+            return;
+        timer = Time.time + timerResetTime;
+    }
+
+    private IEnumerator MoveTheEnemy()
+    {
+        canMove = false;
+        Enemy.AddForce(Vector3.down * moveAmount, ForceMode2D.Impulse);
+        yield return new WaitForSeconds(timerResetTime);
+        canMove = true;
+    }
+}
